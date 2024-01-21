3f7f3298a655d7f4ef286cc9e5c5d93db4e8a75b
alinakrigel <alina.krigel@gmail.com>
2024-01-21 11:56:47 +0200

    added background picture

---
+++ backgroundData.js
@@ -0,0 +1,13 @@
+export class BackgroundData {
+  constructor(game) {
+    this.game = game;
+    this.width = this.game.width;
+    this.height = this.game.height;
+    this.x = 0;
+    this.y = 0;
+    this.image = document.getElementById("background");
+  }
+  draw(context) {
+    context.drawImage(this.image, this.x, this.y, this.width, this.height);
+  }
+}
